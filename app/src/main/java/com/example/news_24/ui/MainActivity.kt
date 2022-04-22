a91e89d00a4017dbeddcd3d614908c99a4fa863c
devvi <android.devv@mail.ru>
2022-04-22 11:56:29 +0600

    Initial commit

---
+++ app/src/main/java/com/example/news_24/ui/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.news_24.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.news_24.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottomNavigationView = findViewById(R.id.navigation_view)
+        bottomNavigationView.setupWithNavController(bottomNavigationView.findNavController())
+    }
+}
---
+++ app/src/main/java/com/example/news_24/ui/fragments/NewsFragment.kt
@@ -0,0 +1,18 @@
+package com.example.news_24.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.news_24.R
+
+class NewsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+}
