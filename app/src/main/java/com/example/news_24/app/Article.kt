89a8541e51ac1ac4a27156e3d4eedf43c72abafb
devvi <android.devv@mail.ru>
2022-04-22 14:05:03 +0600

    Dao

--- app/src/main/java/com/example/news_24/app/Article.kt
+++ app/src/main/java/com/example/news_24/app/Article.kt
@@ -1,6 +1,14 @@
 package com.example.news_24.app
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "article"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
     val author: String,
     val content: String,
     val description: String,
